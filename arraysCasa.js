c38d06b63451a065311c3d682243543ec6f11f1f
abeamonteiro <abeatrizbmonteiro@gmail.com>
2025-12-04 15:03:44 -0300

    acrescenta typescript

---
+++ arraysCasa.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cores = new Array();
+cores = leia.questionInt("\nDigite cinco cores: ", cores);
+console.log("As cores escolhidas foram: " + cores);
