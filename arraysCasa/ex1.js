e13f196740cdd29a15fc058b2f7bf3979ed03231
abeamonteiro <abeatrizbmonteiro@gmail.com>
2025-12-11 17:37:26 -0300

    organiza dir arraysCasa

--- arraysCasa/ex1.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cores = new Array();
-cores = leia.questionInt("\nDigite cinco cores: ", cores);
-console.log("As cores escolhidas foram: " + cores);
